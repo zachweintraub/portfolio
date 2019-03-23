b0e197024f0eb2417991b8958299bed0a8c74e3e
Zach Weintraub <zachweintraub@gmail.com>
2019-03-22 17:30:53 -0700

    add a class to bold currently selected menu item

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,29 @@ $(document).ready(function() {
   console.log("locked and loaded");
 
   $('#about').click(function() {
+    $('.selected').removeClass('selected');
+    $('#about').addClass('selected');
     $('[id^=section]').addClass('invisible');
     $('#section-about').removeClass('invisible');
   })
 
   $('#projects').click(function() {
+    $('.selected').removeClass('selected');
+    $('#projects').addClass('selected');
     $('[id^=section]').addClass('invisible');
     $('#section-projects').removeClass('invisible');
   })
 
   $('#cv').click(function() {
+    $('.selected').removeClass('selected');
+    $('#cv').addClass('selected');
     $('[id^=section]').addClass('invisible');
     $('#section-cv').removeClass('invisible');
   })
 
   $('#contact').click(function() {
+    $('.selected').removeClass('selected');
+    $('#contact').addClass('selected');
     $('[id^=section]').addClass('invisible');
     $('#section-contact').removeClass('invisible');
   })
